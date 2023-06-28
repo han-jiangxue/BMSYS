cd44d41ea7b59f28db97a69515d691748791a75c
HanJiangXue <2956236425@qq.com>
2023-06-28 11:35:01 +0800

    ci: init

---
+++ src/api/login/index.ts
@@ -0,0 +1,28 @@
+import { request } from "@/utils/service"
+import type * as Login from "./types/login"
+
+/** 获取登录验证码 */
+export function getLoginCodeApi() {
+  return request<Login.LoginCodeResponseData>({
+    url: "/user/captcha",
+    method: "get"
+  })
+}
+
+/** 登录并返回 Token */
+export function loginApi(data: Login.LoginRequestData, headers: any) {
+  return request<Login.LoginResponseData>({
+    url: "/user/login",
+    method: "post",
+    data,
+    headers
+  })
+}
+
+/** 获取用户详情 */
+export function getUserInfoApi() {
+  return request<Login.UserInfoResponseData>({
+    url: "users/info",
+    method: "get"
+  })
+}
---
+++ src/api/login/types/login.ts
@@ -0,0 +1,22 @@
+export interface CaptchaRes {
+  code: number
+  message: string
+  date: string
+  data: Record<string, unknown>
+}
+
+export interface LoginParams {
+  idCardNumber: string
+  password: string
+  rememberMe?: boolean
+  code?: string
+  uuid?: string
+}
+
+export type LoginCodeResponseData = CaptchaRes
+
+export type LoginRequestData = LoginParams
+
+export type LoginResponseData = ApiResponseData<{ token: string }>
+
+export type UserInfoResponseData = ApiResponseData<{ username: string; roles: string[] }>
